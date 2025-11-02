cb65f4d2ef31bcd52496d3c6fbaf84cf555ced65
patrikkusnir1 <78038878+patrikkusnir1@users.noreply.github.com>
2025-11-02 12:58:32 +0300

    learn about var

--- lessons/index.js
+++ lessons/index.js
@@ -1,43 +1,5 @@
-function outerFunction(x) {
-	let y = 10;
-	function innerFunction() {
-		console.log(x + y)
-	}
-	return innerFunction;
+if (true) {
+	var num = 5;
 }
 
-let closure = outerFunction(5);
-closure();
-
-function createCounter() {
-	let count = 0;
-	return function() {
-		count++;
-		return count;
-	}
-}
-
-let counter = createCounter();
-console.log(counter());
-console.log(counter());
-
-function multiply(x) {
-	return function(y) {
-		return x * y
-	}
-}
-
-let double = multiply(2);
-console.log(double(5))
-
-function createIncrementer() {
-	let count = 0;
-	return function() {
-		count++;
-		console.log(count)
-	}
-}
-
-let increment = createIncrementer();
-increment()
-increment()
+console.log(num)
